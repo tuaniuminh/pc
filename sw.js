8b6341f44442d0441367d5b72d6ac6e8ec35f5fb
tutututututu99 <tutututututu99@users.noreply.github.com>
2026-07-13 15:09:40 +0700

    Cap nhat code tu dong

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'pc-flex-cache-v1.1.28';
+const CACHE_NAME = 'pc-flex-cache-v1.1.29';
 const ASSETS = [
     './',
     './index.html',
-    './app.js?v=1.1.28',
-    './styles.css?v=1.1.28',
+    './app.js?v=1.1.29',
+    './styles.css?v=1.1.29',
     './manifest.json',
     './icon-192.png',
     './icon-512.png'
