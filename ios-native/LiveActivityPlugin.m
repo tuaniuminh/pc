b19bdf02be1f0bbc760b59dd8c5c2bab6c83b47a
tutututututu99 <tutututututu99@users.noreply.github.com>
2026-08-31 12:27:57 +0700

    fix(ios): Remove redundant LiveActivityPlugin.m to resolve duplicate interface collision (v1.5.7)

--- ios-native/LiveActivityPlugin.m
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Capacitor/Capacitor.h>
-#import "App-Swift.h"
-
-CAP_PLUGIN(LiveActivityPlugin, "LiveActivityPlugin",
-    CAP_PLUGIN_METHOD(startActivity, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(updateActivity, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(stopActivity, CAPPluginReturnPromise);
-)
