643d57a735a7b55695e6203626ca6532393268be
tutututututu99 <tutututututu99@users.noreply.github.com>
2026-08-31 12:12:39 +0700

    fix(ios): Remove final loadView override and filter source files in Xcode build (v1.5.5)

--- ios-native/MainViewController.swift
+++ ios-native/MainViewController.swift
@@ -2,11 +2,6 @@ import UIKit
 import Capacitor
 
 class MainViewController: CAPBridgeViewController {
-    override func loadView() {
-        super.loadView()
-        bridge?.registerPluginType(LiveActivityPlugin.self)
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
         bridge?.registerPluginType(LiveActivityPlugin.self)
